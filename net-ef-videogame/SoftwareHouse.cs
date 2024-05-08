5d170e81033f7afc6054bf9738a88213d6973f09
enricoforesta <enrico.foresta97@gmail.com>
2024-05-08 15:04:00 +0200

    Aggiungere i file di progetto.

---
+++ net-ef-videogame/SoftwareHouse.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_ef_videogame
+{
+    [Table("software_house")]
+    [Index(nameof(SoftwareHouseId), IsUnique = true)]
+    public class SoftwareHouse
+    {
+        [Key] public int SoftwareHouseId { get; set; }
+        public string? Name { get; set; }
+        public string TaxID { get; set; }
+        public string? City { get; set; }
+        public string? Country { get; set; }
+        [Column("created_at")]
+        public DateTime CreatedAt { get; set; }
+        [Column("update_at")]
+        public DateTime UpdatedAt { get; set; }
+        List<Videogames>? Videogames { get; set; } // N-N
+    }
+}
---
+++ net-ef-videogame/Videogames.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_ef_videogame
+{
+
+    [Table("videogames")]
+    [Index(nameof(VideogamsId), IsUnique = true)]
+    public class Videogames
+    {
+        [Key] public int VideogamsId { get; set; }
+        public string Name { get; set; }
+        public string? Overview { get; set; }
+        [Column("release_date")]
+        public DateOnly ReleaseDate { get; set; }
+        [Column("created_at")]
+        public DateTime CreatedAt { get; set; }
+        [Column("update_at")]
+        public DateTime UpdatedAt { get;set; }
+
+        List<SoftwareHouse>? SoftwareHouseList { get; set;} // N-N
+    }
+}
